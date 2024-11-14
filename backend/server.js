30772109ba6bce469f18876ceb7ea637bb530d92
thana44 <tanapol418@gmail.com>
2024-11-14 13:22:45 +0700

    add domain

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express()
 app.use(cookieParser())
 app.use(cors({
     credentials:true,
-    origin: ['http://localhost:5173']
+    origin: ['http://localhost:5173', process.env.FRONTEND_HOST]
 }))
 // app.use(bodyParser.json({limit: '30mb', extended: true}))
 // app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
